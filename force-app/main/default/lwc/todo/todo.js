177bf1344c6243e7370ce2676ba5e2cee864d538
unknown <imsiddiquee@gmail.com>
2020-09-27 20:59:21 +0600

    make connection with Apex controller

--- force-app/main/default/lwc/todo/todo.js
+++ force-app/main/default/lwc/todo/todo.js
@@ -1,5 +1,29 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track, wire } from "lwc";
+import getTasks from "@salesforce/apex/ToDoListController.getTasks";
 
 export default class Todo extends LightningElement {
-  greeting = "hello";
+  newTask = "";
+
+  @wire(getTasks)
+  todoTasks;
+
+  updateNewTask(event) {
+    const input = event.currentTarget.value;
+    this.newTask = input;
+  }
+
+  handlerAdd() {
+    this.todoTasks.push({ id: this.todoTasks.length + 1, name: this.newTask });
+    this.newTask = "";
+  }
+
+  handleDelete(event) {
+    const existingTaskIndex = event.target.dataset.task;
+
+    const taskIndex = this.todoTasks.findIndex((task) => {
+      return JSON.stringify(task.id) === existingTaskIndex;
+    });
+
+    this.todoTasks.splice(taskIndex, 1);
+  }
 }
