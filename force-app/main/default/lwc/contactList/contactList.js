13ce693b46612cfedbf0750abe17387dd90b5685
unknown <imsiddiquee@gmail.com>
2020-10-02 01:20:24 +0600

    error handle process, how customized error

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,6 +1,7 @@
 import { LightningElement, wire } from "lwc";
+import { reduceErrors } from "c/ldsUtils";
 
-import CONTACT_OBJECT from "@salesforce/schema/Contact";
+//import CONTACT_OBJECT from "@salesforce/schema/Contact";
 import FIRSTNAME_FIELD from "@salesforce/schema/Contact.FirstName";
 import LASTNAME_FIELD from "@salesforce/schema/Contact.LastName";
 import EMAIL_FIELD from "@salesforce/schema/Contact.Email";
@@ -24,5 +25,9 @@ export default class ContactList extends LightningElement {
 
   get isLoading() {
     return !this.contacts.data && !this.contacts.error;
+  }
+
+  get errors() {
+    return this.contacts.error ? reduceErrors(this.contacts.error) : [];
   }
 }
