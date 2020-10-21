101533f09db64c465c1616d5ce4f0a954a815488
unknown <imsiddiquee@gmail.com>
2020-10-21 11:01:07 +0600

    Add helper file

---
+++ force-app/main/default/lwc/paintApp/paintTool.js
@@ -0,0 +1,12 @@
+import ICONS from "@salesforce/resourceUrl/paintAppImages";
+export default class PaintTool {
+	static TOOL_LINE_ICON = ICONS + "/paintAppImages/line-icon.png";
+	static TOOL_RECTANGLE_ICON = ICONS + "/paintAppImages/tri-icon.png";
+	static TOOL_CIRCLE_ICON = ICONS + "/paintAppImages/circle-icon.png";
+	static TOOL_TRIANGLE_ICON = ICONS + "/paintAppImages/rect-icon.png";
+
+	static TOOL_LINE = "line";
+	static TOOL_RECTANGLE = "rectangle";
+	static TOOL_CIRCLE = "circle";
+	static TOOL_TRIANGLE = "triangle";
+}
