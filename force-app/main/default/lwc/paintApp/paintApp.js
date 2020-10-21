ca662c7a94c844809703fd74295e2004fe6d20bc
unknown <imsiddiquee@gmail.com>
2020-10-21 09:12:07 +0600

    update

--- force-app/main/default/lwc/paintApp/paintApp.js
+++ force-app/main/default/lwc/paintApp/paintApp.js
@@ -1,3 +1,6 @@
 import { LightningElement } from "lwc";
+import My_Resource from "@salesforce/resourceUrl/paintAppImages";
 
-export default class PaintApp extends LightningElement {}
+export default class PaintApp extends LightningElement {
+	tri_icon = My_Resource + "/paintAppImages/tri-icon.png";
+}
