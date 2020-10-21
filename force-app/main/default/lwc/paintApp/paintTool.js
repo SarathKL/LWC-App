2263724f3d95627baf45a9fb36c0f449cc0479ee
unknown <imsiddiquee@gmail.com>
2020-10-21 11:34:21 +0600

    refactor

--- force-app/main/default/lwc/paintApp/paintTool.js
+++ force-app/main/default/lwc/paintApp/paintTool.js
@@ -1,12 +1,11 @@
 import ICONS from "@salesforce/resourceUrl/paintAppImages";
-export default class PaintTool {
-	static TOOL_LINE_ICON = ICONS + "/paintAppImages/line-icon.png";
-	static TOOL_RECTANGLE_ICON = ICONS + "/paintAppImages/tri-icon.png";
-	static TOOL_CIRCLE_ICON = ICONS + "/paintAppImages/circle-icon.png";
-	static TOOL_TRIANGLE_ICON = ICONS + "/paintAppImages/rect-icon.png";
 
-	static TOOL_LINE = "line";
-	static TOOL_RECTANGLE = "rectangle";
-	static TOOL_CIRCLE = "circle";
-	static TOOL_TRIANGLE = "triangle";
-}
+export const TOOL_LINE_ICON = ICONS + "/paintAppImages/line-icon.png";
+export const TOOL_RECTANGLE_ICON = ICONS + "/paintAppImages/tri-icon.png";
+export const TOOL_CIRCLE_ICON = ICONS + "/paintAppImages/circle-icon.png";
+export const TOOL_TRIANGLE_ICON = ICONS + "/paintAppImages/rect-icon.png";
+
+export const TOOL_LINE = "line";
+export const TOOL_RECTANGLE = "rectangle";
+export const TOOL_CIRCLE = "circle";
+export const TOOL_TRIANGLE = "triangle";
